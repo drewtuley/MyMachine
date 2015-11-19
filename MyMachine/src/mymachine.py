6b4c52e257606965547ea65082da7c30b60da8f9
andrew.tuley <andrew.tuley@wks1230.bjss.co.uk>
2015-11-19 13:22:39 +0000

    initial cut

---
+++ MyMachine/src/mymachine.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="andrew.tuley"
+__date__ ="$19-Nov-2015 13:21:49$"
+
+if __name__ == "__main__":
+    print ("Hello World")
